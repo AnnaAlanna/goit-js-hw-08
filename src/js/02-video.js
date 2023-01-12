db358de6bfa400e08f505027e7db6b41e9442c21
AnnaAlanna <povzunanna@gmail.com>
2023-01-12 23:31:53 +0200

    edited task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,14 +3,28 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
+const saveTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+console.log(saveTime);
+console.log(localStorage);
+setTime();
 
 player.on(
   'timeupdate',
   throttle(function ({ seconds }) {
     localStorage.setItem('videoplayer-current-time', seconds);
   }, 1000)
-  
+
 );
-const saveTime = JSON.parse(localStorage.getItem('videoplayer-current-time'))
-console.log(saveTime);
-player.setCurrentTime(saveTime);
+
+function setTime() {
+    if (localStorage.length === 0) {
+      console.log('start')
+      player.setCurrentTime(0);
+      return;
+    } else {
+      console.log('continue')
+      player.setCurrentTime(saveTime);
+    return;
+   }
+};
+  
